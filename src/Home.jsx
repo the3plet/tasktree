250da4c18d753d2716ae1854e7d33d35e1e24eca
the3plet <soorajp2002@gmail.com>
2024-10-09 02:33:50 +0530

    Functionality completed

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,26 +1,28 @@
-import { Center, HStack, Stack, Text} from "@chakra-ui/react"
-import packageData from "./Data"
-import Task from "./components/Task"
+import { Center, Container, HStack, Stack, Text } from "@chakra-ui/react";
+import packageData from "./Data";
+import Task from "./components/Task";
 const Home = () => {
+  const rootTask = packageData.servicePackageTasks.filter(
+    (tasks) => tasks.parentServicePackageTaskId == null
+  );
   return (
-  
-   <Center height='100vh'>
-    <Stack>
+    <Center height="100vh" width='100vw'>
+      <Stack w='50vw'>
+        <Text>{packageData.title}</Text>
+        <Text>{packageData.description}</Text>
+        <HStack>
+          <Text>Price</Text>
+          <Text>{packageData.price}</Text>
+          <Text>{packageData.currency}</Text>
+        </HStack>
+        <Container>
+          {rootTask.map((task) => (
+            <Task key={task.id} task={task} />
+          ))}
+        </Container>
+      </Stack>
+    </Center>
+  );
+};
 
-<Text>{packageData.title}</Text>
-<Text>{packageData.description}</Text>
-
-    <HStack>
-
-    <Text>Price</Text>
-    <Text>{packageData.price}</Text>
-    <Text>{packageData.currency}</Text>
-    </HStack>
-<Task/>
-    </Stack>
-   </Center>
- 
-  )
-}
-
-export default Home
+export default Home;
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,8 +1,41 @@
+/* eslint-disable react/prop-types */
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Text,
+} from "@chakra-ui/react";
+import packageData from "../Data";
 
-const Task = () => {
+const Task = ({ task, depth = 0 }) => {
+  const childTasks = packageData.servicePackageTasks.filter(
+    (eachTasks) => eachTasks.parentServicePackageTaskId == task.id
+  );
+
   return (
-    <div>Task</div>
-  )
-}
-
-export default Task
+    <Accordion allowToggle>
+      <AccordionItem>
+        <h2>
+          <AccordionButton>
+            <Box as="span" flex="1" textAlign="left">
+              {task.title}
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4}>
+          {task.description ? (
+            <Text dangerouslySetInnerHTML={{ __html: task.description }}></Text>
+          ) : null}
+          {childTasks.map((childtask) => (
+            <Task key={childtask.id} task={childtask} depth={depth + 1} />
+          ))}
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
+  );
+};
+export default Task;
